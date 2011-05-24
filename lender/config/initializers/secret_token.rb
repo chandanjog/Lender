4bb894845bc2a549e05b76a15552970bb3d52f16
Chandan Jog <jog.chandan@gmail.com>
2011-05-25 00:21:00 +0530

    <cj> regenerated the app skeleton with ruby 1.8.7 and added rake 0.8.7 as dependency

--- lender/config/initializers/secret_token.rb
+++ lender/config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Lender::Application.config.secret_token = 'dea5df0ce5c75308918d4b4746b19c1f6224115f4951e884bc0e742d3f782fabdd7951fac4218fb05499c52219d52a476c6fe7c9a39380a68d404f1cc5b3b05d'
+Lender::Application.config.secret_token = 'e694d0582a7fb436d785f684a73346100203e6c2b81bf898d4acdc2fb0d6b76da55a30fd69710be7d26d1299325cbbac18212335c8d133c58e8a86979d9ed940'
