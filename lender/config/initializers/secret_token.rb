c29c36a9f8d38d9e86f448da2b8a9351e44f162b
Chandan Jog <jog.chandan@gmail.com>
2011-05-24 22:54:02 +0530

    <cj> adding fresh rails app

---
+++ lender/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lender::Application.config.secret_token = 'dea5df0ce5c75308918d4b4746b19c1f6224115f4951e884bc0e742d3f782fabdd7951fac4218fb05499c52219d52a476c6fe7c9a39380a68d404f1cc5b3b05d'
